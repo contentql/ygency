050acd0d7d27c366e5457bf186aa64d920f18aba
charanm927 <charanm927@gmail.com>
2024-07-08 22:53:46 +0530

    chore: update about page img anchor tags

--- src/payload/blocks/About/Banner/index.tsx
+++ src/payload/blocks/About/Banner/index.tsx
@@ -1,4 +1,5 @@
 import { BannerType, Media } from '@payload-types'
+import Image from 'next/image'
 
 const Banner = (data: BannerType) => {
   return (
@@ -11,23 +12,29 @@ const Banner = (data: BannerType) => {
             <h1 className='hero-title mb-100 rmb-50 wow fadeInUp delay-0-2s'>
               {data?.title?.split(' ')?.slice(0, 3)?.join(' ')}
               {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
+              <Image
                 className='mxw-20'
                 src={(data?.banner_image1 as Media)?.url as string}
                 alt='title'
+                height={98}
+                width={220}
               />
               {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
+              <Image
                 className='mxw-40'
                 src={(data?.banner_image2 as Media)?.url as string}
                 alt='title'
+                height={100}
+                width={420}
               />
               {data?.title?.split(' ')?.slice(3)?.join(' ')}
               <span className='arrow'>
-                <img
+                <Image
                   className='wow fadeInLeft delay-0-6s'
-                  src='assets/images/hero/title-arrow.png'
+                  src='/assets/images/hero/title-arrow.png'
                   alt='Arrow'
+                  height={8}
+                  width={440}
                 />
               </span>
             </h1>
--- src/payload/blocks/About/VideoArea/index.tsx
+++ src/payload/blocks/About/VideoArea/index.tsx
@@ -1,4 +1,5 @@
 import { Media, VideoAreaType } from '@payload-types'
+import Image from 'next/image'
 import Link from 'next/link'
 
 const VideoArea = (data: VideoAreaType) => {
@@ -6,13 +7,15 @@ const VideoArea = (data: VideoAreaType) => {
     <div className='video-area-two rel z-1'>
       <div className='container-fluid'>
         <div className='video-part style-two wow fadeInUp delay-0-2s mb-40'>
-          <img
+          <Image
             src={(data?.video_image as Media)?.url || ''}
             alt={(data?.video_image as Media)?.alt || ''}
+            height={605}
+            width={1380}
           />
-          <a href={data?.video_link!} className='mfp-iframe video-play'>
+          <Link href={data?.video_link!} className='mfp-iframe video-play'>
             <i className='fas fa-play' />
-          </a>
+          </Link>
         </div>
       </div>
       <div className='container'>
@@ -25,28 +28,34 @@ const VideoArea = (data: VideoAreaType) => {
               <div className='col-xl-10'>
                 <div className='client-logo-wrap'>
                   <Link legacyBehavior href='/contact'>
-                    <a className='client-logo-item'>
-                      <img
-                        src='assets/images/client-logos/client-logo-two1.png'
+                    <span className='client-logo-item'>
+                      <Image
+                        src='/assets/images/client-logos/client-logo-two1.png'
                         alt='Client Logo'
+                        height={85}
+                        width={90}
                       />
-                    </a>
+                    </span>
                   </Link>
                   <Link legacyBehavior href='/contact'>
-                    <a className='client-logo-item'>
-                      <img
-                        src='assets/images/client-logos/client-logo-two2.png'
+                    <span className='client-logo-item'>
+                      <Image
+                        src='/assets/images/client-logos/client-logo-two2.png'
                         alt='Client Logo'
+                        height={85}
+                        width={90}
                       />
-                    </a>
+                    </span>
                   </Link>
                   <Link legacyBehavior href='/contact'>
-                    <a className='client-logo-item'>
-                      <img
-                        src='assets/images/client-logos/client-logo-two3.png'
+                    <span className='client-logo-item'>
+                      <Image
+                        src='/assets/images/client-logos/client-logo-two3.png'
                         alt='Client Logo'
+                        height={85}
+                        width={90}
                       />
-                    </a>
+                    </span>
                   </Link>
                 </div>
               </div>
@@ -55,10 +64,12 @@ const VideoArea = (data: VideoAreaType) => {
           <div className='col-lg-6 wow fadeInRight delay-0-2s'>
             <div className='video-year text-lg-end rel text-center'>
               1980
-              <img
+              <Image
                 className='leaf-shape'
-                src='assets/images/video/leaf.png'
+                src='/assets/images/video/leaf.png'
                 alt='Leaf'
+                height={59}
+                width={99}
               />
             </div>
           </div>
